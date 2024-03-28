2cf98202a09481dd2a1ddf4128b8391221957cb5
Felix Mutuma <felicmutuma8@gmail.com>
2024-03-28 17:28:54 +0300

    commits phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let cost;
+  if (feet<=400) {
+    cost= "This one is on me!"
+  } 
+  else if(feet<=2000){
+    cost= "That will be twenty bucks."
+  } else if(feet>2000 && feet<2500){
+    cost= "I will gladly take your thirty bucks."
+  } else if(feet>2500){
+    cost= "No can do."
+  }
+ return cost;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let response;
+  if (city=="NYC") {
+    response="Ok, sounds good."
+  } else {
+    response="No go."
+  }
+  return response;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+       return "Thank you."
+        break;
+    default:
+      return "Bye."
+      break;
+  }
 }
